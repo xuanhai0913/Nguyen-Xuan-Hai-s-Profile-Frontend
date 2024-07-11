b908bbc418713531d92801e30b7e979801dae235
xuanhai0913 <73636134+xuanhai0913@users.noreply.github.com>
2024-07-11 17:31:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -10,26 +10,45 @@ function scrollFunction() {
   }
 }
 
+
+const navbar = document.getElementById("navbar");
+const body = document.body;
 const navToggle = document.querySelector(".nav-toggle");
 const navLinks = document.querySelectorAll(".nav__link");
+const themeToggleBtn = document.getElementById('theme-toggle-btn');
 
+// Sự kiện cuộn trang
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (window.scrollY > 100) {
+    navbar.classList.add("navbar-scrolled");
+    navbar.classList.remove("navbar-transparent");
+  } else {
+    navbar.classList.remove("navbar-scrolled");
+    navbar.classList.add("navbar-transparent");
+  }
+}
+
+// Sự kiện toggle nav
 navToggle.addEventListener("click", () => {
   document.body.classList.toggle("nav-open");
 });
 
+// Đóng nav khi click vào link
 navLinks.forEach((link) => {
   link.addEventListener("click", () => {
     document.body.classList.remove("nav-open");
   });
 });
-const themeToggleBtn = document.getElementById('theme-toggle-btn');
-const body = document.body;
 
+// Sự kiện toggle theme
 themeToggleBtn.addEventListener('click', () => {
   const currentTheme = body.dataset.theme === 'dark' ? 'light' : 'dark';
   body.dataset.theme = currentTheme;
 
-  // Toggle theme colors based on current theme
   if (currentTheme === 'dark') {
     document.documentElement.style.setProperty('--bg-color', '#333333');
     document.documentElement.style.setProperty('--primary-bg', '#333');
@@ -49,31 +68,3 @@ themeToggleBtn.addEventListener('click', () => {
   }
 });
 
-const body = document.body;
-const navbar = document.getElementById('navbar');
-
-function updateHeaderBackground(theme) {
-  if (theme === 'dark') {
-    navbar.style.backgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--header-bg-dark');
-  } else {
-    navbar.style.backgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--header-bg-light');
-  }
-}
-
-// Gọi hàm updateHeaderBackground khi trang được tải và khi thay đổi chủ đề
-document.addEventListener('DOMContentLoaded', function () {
-  updateHeaderBackground(body.dataset.theme);
-
-  const themeToggleBtn = document.getElementById('theme-toggle-btn');
-  themeToggleBtn.addEventListener('click', function () {
-    const currentTheme = body.dataset.theme === 'dark' ? 'light' : 'dark';
-    body.dataset.theme = currentTheme;
-    updateHeaderBackground(currentTheme);
-  });
-});
-
-// Cập nhật màu nền của header khi cuộn trang
-window.addEventListener('scroll', function () {
-  const currentTheme = body.dataset.theme;
-  updateHeaderBackground(currentTheme);
-});

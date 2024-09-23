48a7db74adb5b818a2607827e8c635db6d95ec45
Nguyễn Xuân Hải <73636134+xuanhai0913@users.noreply.github.com>
2024-09-23 09:56:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -69,3 +69,14 @@ themeToggleCheckbox.addEventListener('change', function() {
     toggleSlider.style.transform = 'translateX(64px)';
   }
 });
+const constraints = {
+    'video': true,
+    'audio': true
+}
+navigator.mediaDevices.getUserMedia(constraints)
+    .then(stream => {
+        console.log('Got MediaStream:', stream);
+    })
+    .catch(error => {
+        console.error('Error accessing media devices.', error);
+    });

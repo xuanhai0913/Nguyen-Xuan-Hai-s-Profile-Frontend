2386478f2d5a50479648392c2b8424f97a1ea432
Nguyễn Xuân Hải <73636134+xuanhai0913@users.noreply.github.com>
2024-09-23 10:29:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -89,3 +89,38 @@ function getConnectedDevices(type, callback) {
 }
 
 getConnectedDevices('videoinput', cameras => console.log('Cameras found', cameras));
+// Updates the select element with the provided set of cameras
+function updateCameraList(cameras) {
+    const listElement = document.querySelector('select#availableCameras');
+    listElement.innerHTML = '';
+    cameras.map(camera => {
+        const cameraOption = document.createElement('option');
+        cameraOption.label = camera.label;
+        cameraOption.value = camera.deviceId;
+    }).forEach(cameraOption => listElement.add(cameraOption));
+}
+
+// Fetch an array of devices of a certain type
+async function getConnectedDevices(type) {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    return devices.filter(device => device.kind === type)
+}
+
+// Get the initial set of cameras connected
+const videoCameras = getConnectedDevices('videoinput');
+updateCameraList(videoCameras);
+
+// Listen for changes to media devices and update the list accordingly
+navigator.mediaDevices.addEventListener('devicechange', event => {
+    const newCameraList = getConnectedDevices('video');
+    updateCameraList(newCameraList);
+});
+// Set up an asynchronous communication channel that will be
+// used during the peer connection setup
+const signalingChannel = new SignalingChannel(remoteClientId);
+signalingChannel.addEventListener('message', message => {
+    // New message from remote client received
+});
+
+// Send an asynchronous message to the remote client
+signalingChannel.send('Hello!');
